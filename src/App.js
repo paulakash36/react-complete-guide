7c164e51a5c8d2b37bb91a5dc37a3d3dea86ad7d
paulakash36 <paulakash36@gmail.com>
2020-09-13 02:27:59 +0530

    Outputting Dynamic Content

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <Person />
+        <Person />
+        <Person />
       </div>
  
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return <p>This is a Person</p>
+    return <p>This is a Person and is {Math.floor(Math.random() *30)} years old...</p>
 }
 
 export default person;
