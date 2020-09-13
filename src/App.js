e979736fd56f6fccc73cfdb0bb798d2b40bbb4c5
paulakash36 <paulakash36@gmail.com>
2020-09-14 03:44:49 +0530

    Using useState() hooks for State Management

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-class App extends Component {
-  state = {
+const app = () => {
+  
+  const [personState, setPersonState] = useState({
     person: [
       {name:"Akash", age:"22"},
       {name:"Kiran", age:"25"},
       {name:"sTRYKzEr", age:"23"}
     ],
-    otherState : "Some other state"
-  }
+  } );
 
-  switchNameHandler = () => {
-    // console.log("Button was clicked...");
-    this.setState({
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personState,otherState);
+
+  const switchNameHandler = () => {
+    setPersonState({
       person: [
         {name:"AkashPaul", age:"22"},
         {name:"Kiran", age:"25"},
@@ -23,21 +26,25 @@ class App extends Component {
     });
   }
 
-  render() {
+  
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <br />
-        <button onClick = {this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age}>My Hobby: Gaming</Person>
+        <button onClick = {switchNameHandler}>Switch name</button>
+        <Person name={personState.person[0].name} age={personState.person[0].age}/>
+        <Person name={personState.person[1].name} age={personState.person[1].age}/>
+        <Person name={personState.person[2].name} age={personState.person[2].age}>My Hobby: Gaming</Person>
       </div>
  
     );
   }
-}
 
-export default App;
+
+export default app;
+ 
+
+
